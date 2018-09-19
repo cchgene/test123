a403f4314149640ca38046b583736c2870aa02da
cchgene <33175858+cchgene@users.noreply.github.com>
2018-09-19 11:12:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -91,14 +91,36 @@ def handle_message(event):
         name_active = str(name)+':'+str(event.message.text)
         line_bot_api.push_message(os.environ['gene_uid'], TextSendMessage(text=name_active))
         line_bot_api.reply_message(event.reply_token,message)
+
+    elif event.message.text[0:5] == '買當季蔬果':
+        if event.message.text[5] == '大':
+            combo_product = event.message.text[1:8]
+            combo_count = event.message.text[-1]
+        elif event.message.text[5] == '中':
+            combo_product = event.message.text[1:8]
+            combo_count = event.message.text[-1]
+        elif event.message.text[5] == '小':
+            combo_product = event.message.text[1:8]
+            combo_count = event.message.text[-1]
+        dic = {'userid':uid,
+            'username':name,
+            'creattime':datetime.now(),
+            'product':combo_product,
+            'count':combo_count,
+            'status':0}
+        mongodb.upsert_one(dic,'vproduct')
+        message = TextSendMessage(text='小鮮盒已經收到囉~~還要其他的嗎?')
+        name_active = str(name)+':'+str(event.message.text)
+        line_bot_api.push_message(os.environ['gene_uid'], TextSendMessage(text=name_active))
+        line_bot_api.reply_message(event.reply_token,message)
         
     elif event.message.text == '查詢訂單':
         product_list = mongodb.get_user_product(uid,'vproduct')
         if len(product_list) == 0:
             message = TextSendMessage(text='目前並無商品')
         else:
-            #message = ','.join(set(product_list))
-            message = str(dict(product_list))
+            message = '\n'.join(('目前購買 '+i[0]+' 共 '+i[1]+' 份' for i in dict(product_list).items()))
+            #message = str(dict(product_list))
             print(message)
             message = TextSendMessage(text=message)
 
@@ -165,6 +187,36 @@ def handle_message(event):
             )
         )
         line_bot_api.reply_message(event.reply_token,message)
+
+    elif event.message.text == '當季蔬果組': 
+        message = TemplateSendMessage(
+                alt_text='需要哪種當季蔬果組合呢?',
+                template=CarouselTemplate(
+                    columns=[
+                        CarouselColumn(
+                        thumbnail_image_url='https://i.imgur.com/qnACcLd.jpg',
+                        title='需要哪種當季蔬果組合呢?',
+                        text='包含當季葉菜、根莖、菇菌、瓜果、豆以及(蔥、薑、蒜、辣椒)調味類',
+                        actions=[
+                            MessageTemplateAction(
+                            label='大組合-299元(4-5人份)',
+                            text='買當季蔬果大組合X1'
+                            ),
+                            MessageTemplateAction(
+                            label='中組合-199元(2-3人份)',
+                            text='買當季蔬果中組合X1'
+                            ),
+                            MessageTemplateAction(
+                            label='小組合-99元(1-2人份)',
+                            text='買當季蔬果小組合X1'
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+
     elif event.message.text == '買':
         message = TemplateSendMessage(
             alt_text='買什麼東西呢?',
